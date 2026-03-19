b95eada355c5911f360c4a23d14300c6d177c6d7
André Deomondes <andredeomondes.dev@gmail.com>
2026-03-19 11:04:49 -0300

    feat: Creates the initial structure in MVC pattern + ConsoleVisual for visual standardization in the terminal.

---
+++ stay-hard-system-v1.0/src/StayHardApp.java
@@ -0,0 +1,33 @@
+import domain.utils.ConsoleVisual;
+
+public class StayHardApp {
+    public static void main(String[] args) {
+
+        /*
+        ==== STAY HARD SYSTEM ====
+
+                Day: 1
+        Streak: 0
+
+        1 - Criar hábito
+        2 - Listar hábitos
+        3 - Completar hábito
+        4 - Ver status
+        5 - Finalizar dia
+        0 - Sair
+*/
+
+        // Criar o menu principal
+
+        // Cabeçalho
+        ConsoleVisual.printHeader("Stay Hard System");
+        ConsoleVisual.info("Loading application...");
+
+
+
+
+
+
+
+    }
+}
